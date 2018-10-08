5fb4574a4a9eee3c311a4867a62ecc360acbe1e3
Martín Pane <martintayx@gmail.com>
2018-10-08 19:49:50 +0100

    Started to sketch the Console for Graphy

---
+++ Assets/Tayx/Graphy - Ultimate Stats Monitor/Scripts/Console/ConsoleManager.cs
@@ -0,0 +1,28 @@
+﻿/* ---------------------------------------
+ * Author: Martin Pane (martintayx@gmail.com) (@tayx94)
+ * Project: Graphy - Ultimate Stats Monitor
+ * Date: 08-Oct-18
+ * Studio: Tayx
+ * This project is released under the MIT license.
+ * Attribution is not required, but it is always welcomed!
+ * -------------------------------------*/
+
+using UnityEngine;
+using System.Collections;
+
+namespace Tayx.Graphy.Console
+{
+    public class ConsoleManager : MonoBehaviour
+    {
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
