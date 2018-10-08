6f546a6d57feb7df0da3bc5ed77436b5bd2494c5
MostHated <m374llic4@gmail.com>
2018-10-08 16:50:13 -0500

    Assisting with Graphys console manager (#20)
    Assisting with Graphys console manager

--- Assets/Tayx/Graphy - Ultimate Stats Monitor/Scripts/Console/ConsoleManager.cs
+++ Assets/Tayx/Graphy - Ultimate Stats Monitor/Scripts/Console/ConsoleManager.cs
@@ -1,6 +1,7 @@
-﻿/* ---------------------------------------
+/* ---------------------------------------
  * Author: Martin Pane (martintayx@gmail.com) (@tayx94)
  * Project: Graphy - Ultimate Stats Monitor
+ * Collaborators:  (mosthated@instance.id) (@MostHated)
  * Date: 08-Oct-18
  * Studio: Tayx
  * This project is released under the MIT license.
@@ -8,21 +9,79 @@
  * -------------------------------------*/
 
 using UnityEngine;
+
 using System.Collections;
+
+using Tayx.Graphy.UI;
 
 namespace Tayx.Graphy.Console
 {
-    public class ConsoleManager : MonoBehaviour
+    public class ConsoleManager : MonoBehaviour, IMovable
     {
 
-        void Start()
+        #region Private Variables
+
+        private GraphyManager           m_graphyManager;
+
+        private ConsoleView             m_consoleView;
+
+        #endregion
+
+        #region Unity Methods
+
+        private void Awake()
+        {
+            Init();
+        }
+
+        private void Start()
+        {
+            UpdateParameters();
+        }
+
+        private void OnEnable()
+        {
+            Application.logMessageReceived += HandleLog;
+        }
+
+        private void OnDisable()
+        {
+            Application.logMessageReceived -= HandleLog;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void SetPosition(GraphyManager.ModulePosition newModulePosition)
+        {
+            m_consoleView.SetPosition(newModulePosition);
+        }
+
+
+        public void UpdateParameters()
         {
 
         }
 
-        void Update()
-        {
+        #endregion
 
+        #region Private Methods
+
+        private void Init()
+        {
+            m_graphyManager = transform.root.GetComponentInChildren<GraphyManager>();
+
+            m_consoleView = GetComponent<ConsoleView>();
         }
+
+        
+
+        private void HandleLog(string logString, string stackTrace, LogType type)
+        {
+            m_consoleView.UpdateLogTextValue(type.ToString() + ": " + logString);
+        }
+
+        #endregion
     }
 }
