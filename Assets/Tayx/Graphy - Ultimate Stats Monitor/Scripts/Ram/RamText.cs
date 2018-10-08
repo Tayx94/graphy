48f1a9f062de2efb652168ce541d937425463725
Lars Aalbertsen <rockylars@hotmail.com>
2018-10-08 11:44:11 +0200

    Added spacing to RamText

--- Assets/Tayx/Graphy - Ultimate Stats Monitor/Scripts/Ram/RamText.cs
+++ Assets/Tayx/Graphy - Ultimate Stats Monitor/Scripts/Ram/RamText.cs
@@ -1,8 +1,10 @@
 ﻿/* ---------------------------------------
- * Author: Martin Pane (martintayx@gmail.com) (@tayx94)
- * Project: Graphy - Ultimate Stats Monitor
- * Date: 05-Dec-17
- * Studio: Tayx
+ * Author:          Martin Pane (martintayx@gmail.com) (@tayx94)
+ * Collaborators:   Lars Aalbertsen (@Rockylars)
+ * Project:         Graphy - Ultimate Stats Monitor
+ * Date:            05-Dec-17
+ * Studio:          Tayx
+ * 
  * This project is released under the MIT license.
  * Attribution is not required, but it is always welcomed!
  * -------------------------------------*/
@@ -19,16 +21,20 @@ using Tayx.Graphy.Utils.NumString;
 namespace Tayx.Graphy.Ram
 {
     public class RamText : MonoBehaviour
-    { 
-        #region Private Variables
-
-        private GraphyManager m_graphyManager;
-
-        private RamMonitor m_ramMonitor;
+    {
+        #region Variables -> Serialized Private
 
         [SerializeField] private Text m_allocatedSystemMemorySizeText;
         [SerializeField] private Text m_reservedSystemMemorySizeText;
         [SerializeField] private Text m_monoSystemMemorySizeText;
+
+        #endregion
+
+        #region Variables -> Private
+
+        private GraphyManager m_graphyManager;
+
+        private RamMonitor m_ramMonitor;
 
         private float m_updateRate = 4f;  // 4 updates per sec.
 
@@ -38,7 +44,7 @@ namespace Tayx.Graphy.Ram
 
         #endregion
 
-        #region Unity Methods
+        #region Methods -> Unity Callbacks
 
         void Awake()
         {
@@ -49,41 +55,44 @@ namespace Tayx.Graphy.Ram
         {
             m_deltaTime += Time.unscaledDeltaTime;
 
-            if (m_deltaTime > 1.0 / m_updateRate)
+            if (m_deltaTime > 1f / m_updateRate)
             {
                 // Update allocated, mono and reserved memory
+                m_allocatedSystemMemorySizeText .text = m_ramMonitor.AllocatedRam.ToStringNonAlloc(m_memoryStringFormat);
+                m_reservedSystemMemorySizeText  .text = m_ramMonitor.ReservedRam.ToStringNonAlloc(m_memoryStringFormat);
+                m_monoSystemMemorySizeText      .text = m_ramMonitor.MonoRam.ToStringNonAlloc(m_memoryStringFormat);
 
-                m_allocatedSystemMemorySizeText.text = m_ramMonitor.AllocatedRam.ToStringNonAlloc(m_memoryStringFormat);
-                m_reservedSystemMemorySizeText.text = m_ramMonitor.ReservedRam.ToStringNonAlloc(m_memoryStringFormat);
-                m_monoSystemMemorySizeText.text = m_ramMonitor.MonoRam.ToStringNonAlloc(m_memoryStringFormat);
-
-                m_deltaTime = 0;
+                m_deltaTime                     = 0f;
             }
         }
 
         #endregion
         
-        #region Public Methods
+        #region Methods -> public
         
         public void UpdateParameters()
         {
-            m_allocatedSystemMemorySizeText.color = m_graphyManager.AllocatedRamColor;
-            m_reservedSystemMemorySizeText.color = m_graphyManager.ReservedRamColor;
-            m_monoSystemMemorySizeText.color = m_graphyManager.MonoRamColor;
+            m_allocatedSystemMemorySizeText .color = m_graphyManager.AllocatedRamColor;
+            m_reservedSystemMemorySizeText  .color = m_graphyManager.ReservedRamColor;
+            m_monoSystemMemorySizeText      .color = m_graphyManager.MonoRamColor;
 
-            m_updateRate = m_graphyManager.RamTextUpdateRate;
+            m_updateRate                    = m_graphyManager.RamTextUpdateRate;
         }
         
         #endregion
 
-        #region Private Methods
+        #region Methods -> Private
 
         private void Init()
         {
             //TODO: Replace this with one activated from the core and figure out the min value.
             if (!FloatString.Inited || FloatString.MinValue > -1000f || FloatString.MaxValue < 16384f)
             {
-                FloatString.Init(-1001f, 16386f);
+                FloatString.Init
+                (
+                    minNegativeValue: -1001f,
+                    maxPositiveValue: 16386f
+                );
             }
 
             m_graphyManager = transform.root.GetComponentInChildren<GraphyManager>();
