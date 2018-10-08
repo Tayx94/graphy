3bc3093d36c7a69c37a411846101138ee52f9bb9
Dan Termani <m374llic4@gmail.com>
2018-10-08 15:15:55 -0500

    Assisting with Graphys console manager
    Assisting with Graphys console manager

---
+++ Assets/Tayx/Graphy - Ultimate Stats Monitor/Scripts/Console/ConsoleView.cs
@@ -0,0 +1,96 @@
+﻿/* ---------------------------------------
+ * Author: Martin Pane (martintayx@gmail.com) (@tayx94)
+ * Project: Graphy - Ultimate Stats Monitor
+ * Collaborators:  (mosthated@instance.id) (@MostHated)
+ * Date: 08-Oct-18
+ * Studio: Tayx
+ * This project is released under the MIT license.
+ * Attribution is not required, but it is always welcomed!
+ * -------------------------------------*/
+
+using UnityEngine;
+using UnityEngine.UI;
+
+using System.Collections;
+using System.Collections.Generic;
+
+using Tayx.Graphy.UI;
+
+namespace Tayx.Graphy.Console
+{
+    public class ConsoleView : MonoBehaviour, IMovable
+    {
+        #region Private Variables
+
+        private RectTransform                   m_rectTransform;
+
+        [SerializeField] private List<Image>    m_backgroundImages = new List<Image>();
+
+        [SerializeField] private Text           m_logText;
+
+        private GraphyManager.ModulePosition    m_modulePosition;
+
+        #endregion
+
+        #region Unity Methods
+
+        private void Awake()
+        {
+            Init();
+        }
+
+        private void Start()
+        {
+            UpdateParameters();
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        // TODO: Pending implementation
+        public void SetPosition(GraphyManager.ModulePosition newModulePosition)
+        {
+            switch (newModulePosition)
+            {
+                case GraphyManager.ModulePosition.TOP_LEFT:
+
+                    break;
+
+                case GraphyManager.ModulePosition.TOP_RIGHT:
+
+                    break;
+
+                case GraphyManager.ModulePosition.BOTTOM_LEFT:
+
+                    break;
+
+                case GraphyManager.ModulePosition.BOTTOM_RIGHT:
+
+                    break;
+            }
+        }
+
+
+        public void UpdateParameters()
+        {
+
+        }
+
+        public void UpdateLogTextValue(string newText)
+        {
+            m_logText.text = newText;
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void Init()
+        {
+            
+        }
+
+        #endregion
+    }
+}
