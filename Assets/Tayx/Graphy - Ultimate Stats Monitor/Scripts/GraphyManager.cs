55bff5d1251c65022d3e8f4000cca11878b325cf
Lars Aalbertsen <rockylars@hotmail.com>
2018-10-09 09:26:11 +0200

    Finalizing the last few files

--- Assets/Tayx/Graphy - Ultimate Stats Monitor/Scripts/GraphyManager.cs
+++ Assets/Tayx/Graphy - Ultimate Stats Monitor/Scripts/GraphyManager.cs
@@ -23,6 +23,14 @@ namespace Tayx.Graphy
     //[ExecuteInEditMode]
     public class GraphyManager : Singleton<GraphyManager>
     {
+        /* ----- TODO: ----------------------------
+         * Check if we can seal this class.
+         * Add summaries to the variables.
+         * Add summaries to the functions.
+         * Check if we can remove "using System.Collections;".
+         * Check if we should add "private" to the Unity Callbacks.
+         * --------------------------------------*/
+
         protected GraphyManager () { }
 
         //Enums
