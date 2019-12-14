77016dd5fbf51bdef763e1acbe28a40e26a38685
Andree Yosua <andree.yosua@gmail.com>
2019-12-14 07:45:34 +0700

    Move MenuItem to Editor

---
+++ Scripts/Editor/GraphyMenuItem.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+namespace Tayx.Graphy
+{
+    public class GraphyMenuItem
+    {
+        [MenuItem("Graphy/Import Graphy")]
+        static void ImportGraphy()
+        {
+            AssetDatabase.ImportPackage("Packages/com.tayx.graphy/Import Graphy.unitypackage", true);
+        }
+    }
+}
